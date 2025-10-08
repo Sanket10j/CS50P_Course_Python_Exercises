68dbc510c4e0b6a4a19496b0c24600bd4998b093
SANKET CHAUDHARY <121545162+Sanket10j@users.noreply.github.com>
2025-10-08 18:03:36 +0530

    Add files via upload

---
+++ taqueria.py
@@ -0,0 +1,24 @@
+menu = {
+    "Baja Taco": 4.00,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+x = 0
+while True:
+        try:
+           dish = input('Item:').title()
+           x = x + float(menu[dish])
+           print(f'Total: ${x:.2f}')
+        except KeyError:
+           continue
+        except EOFError:
+           break
+
+
+
